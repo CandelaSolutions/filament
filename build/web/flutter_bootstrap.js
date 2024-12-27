96a07f7589f0d48213d5ee6d5ed77b39594661f1
JDHinds <jude.hinds@gmail.com>
2024-12-27 18:09:43 +0000

    Build Commit

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3789986810"
+    serviceWorkerVersion: "1869915081"
   }
 });
