9512c5cee382c7c50219eed6963e4af625391711
JDHinds <jude.hinds@gmail.com>
2025-01-27 21:36:03 +0000

    Added use of `candela_standards` library

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
+import 'package:candela_standards/candela_standards.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:provider/provider.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 import 'package:filament/Classes/NavigationRail.dart';
@@ -24,95 +21,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => AppState(),
-      child: MaterialApp(
-          title: 'Filament',
-          theme: ThemeData(
-              useMaterial3: true,
-              colorScheme: ColorScheme.dark(
-                  primary: Color.fromARGB(255, 224, 134, 0),
-                  secondary: Color.fromARGB(255, 239, 191, 56))),
-          home: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [
-                  Color.fromARGB(255, 224, 134, 0),
-                  Color.fromARGB(255, 239, 191, 56),
-                ],
-                    stops: [
-                  0,
-                  1
-                ])),
-            child: Column(
-              children: [
-                if (kIsWeb)
-                  SizedBox(
-                    height: 4,
-                  )
-                else if (Platform.isWindows)
-                  WindowTitleBarBox(
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: MoveWindow(
-                                child: Row(
-                          children: [
-                            SizedBox(
-                              width: 30,
-                              child: Icon(Icons.emoji_objects_outlined),
-                            ),
-                            Text("Filament",
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 255, 255, 255),
-                                    fontSize: 14,
-                                    decoration: TextDecoration.none,
-                                    fontWeight: FontWeight.normal))
-                          ],
-                        ))),
-                        WindowButtons()
-                      ],
-                    ),
-                  )
-                else if (Platform.isAndroid)
-                  SizedBox(
-                    height: MediaQuery.of(context).viewPadding.top,
-                  ),
-                Expanded(child: ReadyScene()),
-              ],
-            ),
-          )),
+    return CandelaStandardApp(
+      icon: Icon(Icons.emoji_objects_outlined),
+      text: "Filament",
+      primary: Color.fromARGB(255, 224, 134, 0),
+      secondary: Color.fromARGB(255, 239, 191, 56),
+      child: ReadyScene(),
     );
   }
 }
-
-class WindowButtons extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        MinimizeWindowButton(
-          colors: WindowButtonColors(
-              mouseOver: Color.fromARGB(255, 224, 134, 0),
-              iconNormal: Color.fromARGB(255, 255, 255, 255)),
-        ),
-        MaximizeWindowButton(
-          colors: WindowButtonColors(
-              mouseOver: Color.fromARGB(255, 224, 134, 0),
-              iconNormal: Color.fromARGB(255, 255, 255, 255)),
-        ),
-        CloseWindowButton(
-          colors: WindowButtonColors(
-              iconNormal: Color.fromARGB(255, 255, 255, 255)),
-        )
-      ],
-    );
-  }
-}
-
-class AppState extends ChangeNotifier {}
 
 class ReadyScene extends StatefulWidget {
   @override
